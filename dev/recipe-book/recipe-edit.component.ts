734f27956e8d2158fea37dd919c9d92de23c71f4
M <mritunjay.b.kumar@capgemini.com>
2016-04-17 21:30:51 +0200

    indentation

--- dev/recipe-book/recipe-edit.component.ts
+++ dev/recipe-book/recipe-edit.component.ts
@@ -13,31 +13,20 @@ export class RecipeEditComponent implements OnInit, CanDeactivate {
 	private _editMode = 'create';
 	private _recipeIndex: number;
 	private _submitted = false;
-	constructor(
-		private _routeParams: RouteParams,
-		private _recipeService: RecipeService,
-		private _formBuilder: FormBuilder,
-		private _router: Router
-	) {}
+	constructor(private _routeParams: RouteParams, private _recipeService: RecipeService, private _formBuilder: FormBuilder, private _router: Router){
+
+	}
 	onAddItem(itemName: string, itemAmount: string){
 		(<ControlArray>this.myForm.controls['ingredients']).push(
 			new ControlGroup(
 				{
-					name: new Control(
-						itemName,
-						Validators.required),
-					amount: new Control(
-						itemAmount,
-						Validators.compose([
-							Validators.required,
-							hasNumbers
-						]))
+					name: new Control(itemName,	Validators.required),
+					amount: new Control(itemAmount,	Validators.compose([Validators.required,hasNumbers]))
 				}
 			)
 		)
 	}
 	onRemoveItem(index: number) {
-
 		(<ControlArray>this.myForm.controls['ingredients']).removeAt(index);
 	}
 	onSubmit(){
@@ -58,30 +47,16 @@ export class RecipeEditComponent implements OnInit, CanDeactivate {
 	}
 	ngOnInit():any {
 		this._editMode = this._routeParams.get('editMode');
-		let fbRecipeName = '';
-		let fbRecipeImageUrl = '';
-		let fbRecipeContent = '';
+		let fbRecipeName, fbRecipeImageUrl, fbRecipeContent = '';
 		let fbIngredients: ControlArray = new ControlArray([]);
-
 		if(this._editMode === 'edit'){
 			this._recipeIndex = +this._routeParams.get('itemIndex');
 			this.recipe = this._recipeService.getRecipe(this._recipeIndex);
 			for (let i = 0; i < this.recipe.ingredients.length; i++){
-				fbIngredients.push(
-					new ControlGroup(
-						{
-							name: new Control(
-								this.recipe.ingredients[i].name,
-								Validators.required),
-							amount: new Control(
-								this.recipe.ingredients[i].amount,
-								Validators.compose([
-									Validators.required,
-									hasNumbers
-
-								]))
-						}
-					)
+				fbIngredients.push(new ControlGroup({
+							name: new Control(this.recipe.ingredients[i].name, Validators.required),
+							amount: new Control(this.recipe.ingredients[i].amount,	Validators.compose([Validators.required, hasNumbers]))
+						})
 				);
 				fbRecipeName = this.recipe.name;
 				fbRecipeImageUrl = this.recipe.imageUrl;
@@ -93,7 +68,7 @@ export class RecipeEditComponent implements OnInit, CanDeactivate {
 			imageUrl: [fbRecipeImageUrl],
 			content: [fbRecipeContent],
 			ingredients: this._formBuilder.array(fbIngredients.controls)
-		})
+		});
 	}
 	routerCanDeactivate(nextInstruction: ComponentInstruction, prevInstruction: ComponentInstruction){
 		if(this._submitted || this.myForm.pristine) {
