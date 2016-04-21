17ce24db8779386ce0f0ad45713cef041fa18f02
M <mritunjay.b.kumar@capgemini.com>
2016-04-21 17:11:51 +0200

    kkk

--- dev/rx-js/rx-js.component.ts
+++ dev/rx-js/rx-js.component.ts
@@ -1,16 +1,38 @@
 import {Component} from 'angular2/core';
-import {HTTPRestDataService} from "./http-rest.data.service";
+import {Observable} from "rxjs/Observable";
 
 @Component({
     template: `
      <H1> Hellp RX JS Example</H1>
+     <button (click)="onStart()">Start</button> ||||  <button (click)="onSubscribe()">onSubscribe</button>
     `
-
 })
 export class RXJSComponent {
     getData:string;
     error:any;
-    constructor() {
+    private data: Observable<Array<any>>;
+    constructor() { }
+
+    onStart(){
+        this.data = new Observable(
+            observer => {
+                setTimeout(
+                    () => {
+                        observer.next(42);
+                        observer.next(43);
+                        observer.complete();
+                    },200);
+                console.log('Started Observer Sequence onStart');
+            });
+    }
+
+    onSubscribe(){
+        let subscription = this.data.subscribe(
+            value => console.log('value : ' + value),
+            error => console.log('error : ' + error),
+            () => console.log('Completed Observer sequence')
+        );
+
     }
 
 }
