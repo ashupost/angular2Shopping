39fe3208854a3e73144ee74c66425f0a15bb8b2d
M <mritunjay.b.kumar@capgemini.com>
2016-04-23 17:43:13 +0200

    Commit

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -24,6 +24,7 @@ import {DataService} from "./di/di-data.service";
 @Component({
     selector: 'my-app',
     template: `
+           <p><a [routerLink]="['Recipes']">Recipes</a></p>
   			<router-outlet></router-outlet>
    `,
     directives: [ROUTER_DIRECTIVES],
