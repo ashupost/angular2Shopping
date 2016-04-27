34d61dfe80438627792beea1ee051817f7da826d
M <mritunjay.b.kumar@capgemini.com>
2016-04-27 23:18:12 +0200

    fixed ransform

--- dev/pipe-example/truncatepipe.component.ts
+++ dev/pipe-example/truncatepipe.component.ts
@@ -3,15 +3,10 @@ import {Pipe, PipeTransform} from 'angular2/core'
 @Pipe({
     name: 'truncate'
 })
-export class TruncatePipe {
-    transform(value: string, args: string[]) : string {
-
-       // console.log(args.length);
-        // TODO: Need to fix args length
+export class TruncatePipe implements PipeTransform{
+    transform(value: string, ...args: string[]) : string {
         let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
         let trail = args.length > 1 ? args[1] : '...';
-
         return value.length > limit ? value.substring(0, limit) + trail : value;
-
     }
 }
